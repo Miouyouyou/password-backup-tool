1ce6d5b14857e7f24ded9ca66bf093da9530279f
JustOff <Off.Just.Off@gmail.com>
2017-06-10 17:15:32 +0300

    Remove injection of button in Thunderbird Preferences panel

--- modules/ui.js
+++ modules/ui.js
@@ -23,69 +23,6 @@ PwdEx.UI = {
   },
 
   /**
-   * Adds the password exporter button to the Thunderbird preferences panel.
-   * @param aDocument the document to insert the button to.
-   */
-  addTBButton : function(aDocument) {
-    this._logger.debug("addTBButton");
-
-    if (null != aDocument.getElementById("securityPrefs")) {
-      let container =
-        aDocument.getElementById("securityPrefs").
-          getElementsByTagName("tabpanel")[3].getElementsByTagName("hbox")[0];
-
-      if (null != container) {
-        container.appendChild(this._createPwdExButton(aDocument));
-      } else {
-        this._logger.error("addTBButton. Container for button not found.");
-      }
-    } else {
-      this._logger.debug(
-        "addTBButton. Security pane not showing; listener added.");
-
-      let secPane = aDocument.getElementById("paneSecurity");
-      let that = this;
-
-      secPane.addEventListener(
-        "paneload",  function(e) { that.addTBButton(aDocument); }, false);
-    }
-  },
-
-  /**
-   * Remove the password exporter button from the Thunderbird preferences panel.
-   * @param aDocument the document to remove the button from.
-   */
-  removeTBButton : function(aDocument) {
-    this._logger.debug("removeTBButton");
-
-    let button = aDocument.getElementById("pwdex-button");
-
-    if (null != button) {
-      button.parentNode.removeChild(button);
-    }
-  },
-
-  /**
-   * Opens the password exporter window.
-   * @param aWindow the parent window.
-   */
-  openPwdExWindow : function(aWindow) {
-    this._logger.debug("openPwdExWindow");
-
-    let win = Services.wm.getMostRecentWindow("passwordexporter-dialog");
-
-    // check if a window is already open.
-    if ((null != win) && !win.closed) {
-      win.focus();
-    } else {
-      aWindow.openDialog(
-        "chrome://pwdbackuptool/content/pwdexDialog.xul",
-        "passwordexporter-dialog",
-        "chrome,centerscreen,dialog,resizable");
-    }
-  },
-
-  /**
    * Opens the file picker for an export operation.
    * @param aWindow the parent window for the file picker.
    * @return object with chosen file and file type, null if no choice was made.
@@ -117,29 +54,6 @@ PwdEx.UI = {
     }
 
     return result;
-  },
-
-  /**
-   * Create the password exporter button node.
-   * @param aDocument the document the create the node in.
-   * @return the node corresponding to the button.
-   */
-  _createPwdExButton : function(aDocument) {
-    this._logger.trace("_createPwdExButton");
-
-    let that = this;
-    let button = aDocument.createElement("button");
-
-    button.setAttribute("id", "pwdex-button");
-    button.setAttribute(
-      "label", PwdEx.getString("passwordexporter.button-label"));
-    button.setAttribute(
-      "accesskey", PwdEx.getString("passwordexporter.button-accesskey"));
-    button.addEventListener(
-      "command",  function(e) { that.openPwdExWindow(aDocument.defaultView); },
-      false);
-
-    return button;
   },
 
   /**
