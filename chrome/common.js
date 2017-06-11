792ddfed6ca3b43394bb373fe7042aa8b1ae9332
JustOff <Off.Just.Off@gmail.com>
2017-06-11 16:04:03 +0300

    Get rid of obsolete and unused log4moz

--- chrome/common.js
+++ chrome/common.js
@@ -6,19 +6,6 @@
 
 var EXPORTED_SYMBOLS = [ "PwdEx" ];
 
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-
-var logLoaded = true;
-
-try {
-  Components.utils.import("resource://gre/modules/services-common/log4moz.js");
-} catch (e) {
-  // certain Mozilla-based applications don't have /services-common/, so let's
-  // just skip logging there.
-  logLoaded = false;
-}
-
 Components.utils.import("resource://gre/modules/Services.jsm");
 
 /**
@@ -30,71 +17,10 @@ if ("undefined" == typeof(PwdEx)) {
      * Initialize this object.
      */
     init : function() {
-      if (logLoaded) {
-        // Setup logging. See http://wiki.mozilla.org/Labs/JS_Modules.
-        // The basic formatter will output lines like:
-        // DATE/TIME  LoggerName LEVEL  (log message)
-        let formatter = new Log4Moz.BasicFormatter();
-        let appender;
-
-        this._logger = Log4Moz.repository.getLogger("PwdEx");
-
-        let logFile = this.getDirectory();
-
-        logFile.append("log.txt");
-        // this appender will log to the file system.
-        if (null != Log4Moz.BoundedFileAppender) {
-          appender = new Log4Moz.BoundedFileAppender(logFile.path, formatter);
-        } else {
-          appender = new Log4Moz.RotatingFileAppender(logFile, formatter);
-        }
-
-        this._logger.level = Log4Moz.Level["All"];
-        appender.level = Log4Moz.Level["Warn"]; // change this to adjust level.
-        this._logger.addAppender(appender);
-      } else {
-        this._logger =
-          { error : function() {}, warn : function() {}, debug : function() {},
-            trace : function() {}, info : function() {} };
-      }
 
       this.stringBundle =
         Services.strings.createBundle(
           "chrome://pwdbackuptool/locale/passwordexporter.properties");
-    },
-
-    /**
-     * Creates a logger for other objects to use.
-     * @param aName the name of the logger to create.
-     * @param aLevel (optional) the logger level.
-     * @return the created logger.
-     */
-    getLogger : function(aName, aLevel) {
-      let logger;
-
-      if (logLoaded) {
-        logger = Log4Moz.repository.getLogger(aName);
-
-        logger.level = Log4Moz.Level[(aLevel ? aLevel : "All")];
-        logger.parent = this._logger;
-      } else {
-        logger =
-          { error : function() {}, warn : function() {}, debug : function() {},
-            trace : function() {}, info : function() {} };
-      }
-
-      return logger;
-    },
-
-    /**
-     * Gets a reference to the directory where this add-on will keep its files.
-     * The directory is created if it doesn't exist.
-     * @return reference (nsIFile) to the directory.
-     */
-    getDirectory : function() {
-      Components.utils.import("resource://gre/modules/FileUtils.jsm");
-
-      return FileUtils.getDir("ProfD", [ "PasswordExporter" ], true);
     },
 
     /**
